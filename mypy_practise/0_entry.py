80b2af51a39c06a24e4333efc4920138ccff96f5
Gao Zheng <ba361006@gmail.com>
2022-11-01 13:58:36 +0800

    update command line

--- mypy_practise/0_entry.py
+++ mypy_practise/0_entry.py
@@ -11,3 +11,7 @@ ages = [10, 20, 30]
 
 greet_all(names)  # Ok!
 greet_all(ages)  # Error due to incompatible types
+
+# to check this file, run mypy mypy_practise/0_entry.py
+# to check mypy_practise folder, run mypy mypy_practise
+# to check subdirectory folder, run mypy mypy_practise/sub_directory/*
